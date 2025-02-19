cdf9b88517cdb1c97cdc47699fec5a41db066233
Semanto Mondal <133217806+semanto-mondal@users.noreply.github.com>
2025-02-19 15:20:34 +0600

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -9,7 +9,7 @@ warnings.filterwarnings('ignore')
 
 #Setting the title and page icon
 st.set_page_config(page_title="WEATHER", page_icon=":sun_behind_rain_cloud:", layout="wide")
-st.title("<h1 style='text-align: center;'>🌦️ Weather Data Analysis</h1>", unsafe_allow_html=True)
+st.title(':partly_sunny_rain: Weather Data Analysis')
 #st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
 
 
